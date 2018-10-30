9c0e2df6befda8f26ccebe620a967ad7f34acc28
kidc12 <kidc12@users.noreply.github.com>
2018-10-30 19:29:54 -0400

    Start menu done

--- app/src/main/java/com/example/jchas/kingsdemise/MainActivity.java
+++ app/src/main/java/com/example/jchas/kingsdemise/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.jchas.kingsdemise;
 
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.headline);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Blox.ttf");
+        textView.setTypeface(typeface);
 
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.redbone);
         mediaPlayer.start();
