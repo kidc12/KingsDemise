2b5c4af5e1126b1f946f32881ad7be80396cd8f8
kidc12 <kidc12@users.noreply.github.com>
2018-10-30 22:12:49 -0400

    Start menu done with font mods

--- app/src/main/java/com/example/jchas/kingsdemise/MainActivity.java
+++ app/src/main/java/com/example/jchas/kingsdemise/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +14,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.headline);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "Blox.ttf");
-        textView.setTypeface(typeface);
+        TextView headline = findViewById(R.id.headline);
+        TextView start = findViewById(R.id.start);
+        TextView settings = findViewById(R.id.settings);
+        TextView credits = findViewById(R.id.credits);
+
+        Typeface font1 = Typeface.createFromAsset(getAssets(), "Blox.ttf");
+        Typeface font2 = Typeface.createFromAsset(getAssets(), "pixelflag.ttf");
+
+        headline.setTypeface(font1);
+        start.setTypeface(font2);
+        settings.setTypeface(font2);
+        credits.setTypeface(font2);
 
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.redbone);
         mediaPlayer.start();
 
+    }
+
+    public void credits(View view) {
+
+    }
+
+    public void settings(View view) {
+    }
+
+    public void start(View view) {
     }
 }
