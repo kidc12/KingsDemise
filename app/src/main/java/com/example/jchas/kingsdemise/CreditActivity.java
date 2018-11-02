390e6c41b14e4cccfcbab273271245107848a9ca
kidc12 <kidc12@users.noreply.github.com>
2018-11-01 21:34:20 -0400

    screens added

--- app/src/main/java/com/example/jchas/kingsdemise/CreditActivity.java
+++ app/src/main/java/com/example/jchas/kingsdemise/CreditActivity.java
@@ -1,11 +1,17 @@
 package com.example.jchas.kingsdemise;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreditActivity extends AppCompatActivity {
 
@@ -13,16 +19,43 @@ public class CreditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("LongLogTag")
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                TextView headline = findViewById(R.id.name_bar);
+                Typeface font1 = Typeface.createFromAsset(getAssets(), "Blox.ttf");
+                headline.setTypeface(font1);
+
+                Log.i("Send email", "");
+                String[] TO = {
+                        "jckitson@loyola.edu"
+                };
+                String[] CC = {
+                        "cmrusso@loyola.edu"
+                };
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Lazy Bison");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Hey tell me about your app!");
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                    finish();
+                    Log.i("Finished sending email...", "");
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(CreditActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
+
 }
