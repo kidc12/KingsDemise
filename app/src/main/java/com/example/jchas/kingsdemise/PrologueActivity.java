b22df4e36107070f2034872e88f43d8a17f67b38
chmrusso <cmrusso@loyola.edu>
2018-12-01 23:22:17 -0500

    added characters and voice

--- app/src/main/java/com/example/jchas/kingsdemise/PrologueActivity.java
+++ app/src/main/java/com/example/jchas/kingsdemise/PrologueActivity.java
@@ -8,6 +8,8 @@ import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,15 +26,19 @@ public class PrologueActivity extends  AppCompatActivity{
     private String[] dialogueKing2;
     private int countKing2;
 
+    private String userName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prologue);
         countKing = 0;
+        countFarm = 0;
+        countKing2 = 0;
         dialogueKing = new String[] {"Yet another run down farm... ▸", "Then I say this ▸", "Along with this ▸", "Abd U can keep talking ▸"};
         dialogueFarm = new String[] {"Hey what do you think you're doing! ▸"};
-        dialogueKing2 = new String[] {"You there! What is your name!? "};
+        dialogueKing2 = new String[] {"You there! What is your name!? ","Oh so your name is " + userName + "?"};
 
     }
 
@@ -46,34 +52,66 @@ public class PrologueActivity extends  AppCompatActivity{
             //set title of person speaking to king
             speech.setText(dialogueKing[countKing]);
             countKing++;
+            return;
         }
 
         if(countFarm < dialogueFarm.length && countKing >= dialogueKing.length){
+            ImageView myImage = (ImageView) findViewById(R.id.farmer_ProID);
+            myImage.setAlpha(1.0f);
             speech.setText(dialogueFarm[countFarm]);
             countFarm++;
+            return;
         }
 
-        if(countKing < dialogueKing2.length){
+        if(countKing2 < dialogueKing2.length && countFarm >= dialogueFarm.length){
             speech.setText(dialogueKing2[countKing2]);
             countKing2++;
 
-            //voice
-            PackageManager manager = getPackageManager();
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            List<ResolveInfo> list = manager.queryIntentActivities( intent, 0);
-            if( list.size() > 0){
-                //listen and report
-                listen(intent);
-            }else {
-                Log.w("VoiceDump", "Error");
+            if(countKing2 == 1){
+                getVoice();
             }
 
+            if(countKing2 == 2){
+                Button yes = (Button) findViewById(R.id.yes_ProID);
+                Button no = (Button) findViewById(R.id.no_ProID);
+
+                yes.setEnabled(true);
+                no.setEnabled(true);
+                yes.setAlpha(1.0f);
+                no.setAlpha(1.0f);
+            }
+
+
+
+            return;
         }
-        
 
 
     }
 
+
+    public void saveName(View button){
+        countKing2 =1;
+        nextLine(button);
+    }
+
+    public void newName(View button){
+        countKing2 = 1;
+        nextLine(button);
+    }
+
+    public void getVoice(){
+        //voice
+        PackageManager manager = getPackageManager();
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        List<ResolveInfo> list = manager.queryIntentActivities( intent, 0);
+        if( list.size() > 0){
+            //listen and report
+            listen(intent);
+        }else {
+            Log.w("VoiceDump", "Error");
+        }
+    }
 
     public void listen( Intent intent){
 
@@ -85,11 +123,12 @@ public class PrologueActivity extends  AppCompatActivity{
         startActivityForResult(intent, 1);
     }
 
-
     protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
         if( requestCode == 1 && resultCode == RESULT_OK ) {
             ArrayList<String> results = data.getStringArrayListExtra( RecognizerIntent.EXTRA_RESULTS );
             float [] scores = data.getFloatArrayExtra( RecognizerIntent.EXTRA_CONFIDENCE_SCORES );
+
+            userName = results.get(0);
 
             int i = 0;
             for( String s : results ) {
